0ddbeec58e3a92acf4cc00cf8947cec2323cffee
divyachn <dvchhn7@gmail.com>
2019-04-19 11:46:36 +0530

    working

--- login.php
+++ login.php
@@ -71,113 +71,121 @@
 
 <body>
 
-<h2 style="margin-left: 30%; margin-top: 5%;">CS315 Project <br/> FIR Management System</h2>
-<img src="./images/police_salute.jpeg" alt="We are always there for you!" height="30%" width="20%" style="margin-left: 8%;">
+  <h2 style="margin-left: 30%; margin-top: 5%;">CS315 Project <br/> FIR Management System</h2>
+  <img src="./images/police_salute.jpeg" alt="We are always there for you!" height="30%" width="20%" style="margin-left: 8%;">
 
-<!--  Login Form -->
-<form style="width:25%; border-style: solid; border-color: blue; border-radius: 5%; padding: 2%; margin-left: 30%; margin-top: -15%;" method="post" action="./login.php">
-  <div>
-    <label for="uname_l"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname_l" required>
-    <label for="psw_l"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw_l" required>
-    <input type="submit">
-  </div>
-  <br/>
-  <b>Create account: </b> </br>
-  <img id="create" src="./images/sign-up.png" alt="We are always there for you!" height="50" width="95" style="margin-left: 40%;" onclick="document.getElementById('id01').style.display='block'">
-</form>
+  <!--  Login Form -->
+  <form style="width:25%; border-style: solid; border-color: blue; border-radius: 5%; padding: 2%; margin-left: 30%; margin-top: -15%;" method="post" action="./login.php">
+    <div>
+      <label for="uname_l"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname_l" required>
+      <label for="psw_l"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw_l" required>
+      <input type="submit">
+    </div>
+    <br/>
+    <b>Create account: </b> </br>
+    <img id="create" src="./images/sign-up.png" alt="We are always there for you!" height="50" width="95" style="margin-left: 40%;" onclick="document.getElementById('id01').style.display='block'">
+  </form>
 
-<!--  Sign-Up form -->
-<form id="id01" class="modal" method="post" action="./login.php">
-  <div>
-    <label for="fname"><b>First Name</b></label>
-    <input type="text" placeholder="Enter First Name" name="fname" required>
+  <!--  Sign-Up form -->
+  <form id="id01" class="modal" method="post" action="./login.php">
+    <div>
+      <label for="fname"><b>First Name</b></label>
+      <input type="text" placeholder="Enter First Name" name="fname" required>
 
-    <label for="lname"><b>Last Name</b></label>
-    <input type="text" placeholder="Enter Last Name" name="lname" required>
+      <label for="lname"><b>Last Name</b></label>
+      <input type="text" placeholder="Enter Last Name" name="lname" required>
 
-    <label for="uname"><b>UserName</b></label>
-    <input type="text" placeholder="Enter User Name" name="uname" required>
+      <label for="uname"><b>UserName</b></label>
+      <input type="text" placeholder="Enter User Name" name="uname" required>
 
-    <label for="mob"><b>Mobile Number</b></label>
-    <input type="text" placeholder="Mobile Number" name="mob" required>
+      <label for="mob"><b>Mobile Number</b></label>
+      <input type="text" placeholder="Mobile Number" name="mob" required>
 
-    <label for="id"><b>Citizen Id</b></label>
-    <input type="text" placeholder="Citizen ID" name="cid" required>
+      <label for="id"><b>Citizen Id</b></label>
+      <input type="text" placeholder="Citizen ID" name="cid" required>
 
-    <label for="mail"><b>Email Id</b></label>
-    <input type="email" placeholder="Enter email" name="mail" required>
+      <label for="mail"><b>Email Id</b></label>
+      <input type="email" placeholder="Enter email" name="mail" required>
 
-    <label for="psw"><b>Enter Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+      <label for="psw"><b>Enter Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <button type="submit">Register</button>
-  </div>
-</form>
+      <button type="submit">Register</button>
+    </div>
+  </form>
 
-<?php
-  // set connection parameters
-  $servername = "localhost";
-  $username = "root";
-  $password = "asd";
-  $db = "FIR_Mgmt";
+  <?php
+    // set connection parameters
+    $servername = "localhost";
+    $username = "root";
+    $password = "asd";
+    $db = "FIRM";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $db);
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_REQUEST['uname'];
-    $firstname = $_REQUEST['fname'];
-    $lastname = $_REQUEST['lname'];
-    $name = $firstname." ".$lastname;
-    $cid = $_REQUEST['cid'];
-    $mob = $_REQUEST['mob'];
-    $dob = $_REQUEST['dob'];
-    $mail = $_REQUEST['mail'];
-    $password = $_REQUEST['psw'];
-    $username_l = $_REQUEST['uname_l'];
-    $password_l = $_REQUEST['psw_l'];
-    if ( !empty($username_l) ){
-      $_SESSION['username'] = $username_l;
-      header("Location: ./query.php");
-      // Login Form
-      $sql = "select Password, Username, PerID from User where Username = " . $username_l . " and Password = " . $password_l . ";";
-      $result = $conn->query($sql);
-      if($result->num_rows == 0){
-        echo "Invalid Username and Password!";
-      }
-      else{
-        while($row = $result->fetch_assoc()) {
-          $perID = $row["PerID"]
-          if ($PerID == 1){
-            // ADMIN
-            header("Location: ./query_admin.php");
-          }
-          elif ($PerID == 2){
-            // Police
-            header("Location: ./query_police.php");
-          }
-          else{
-            // Normal User
-            header("Location: ./query.php");
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Database connected!";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $username = $_REQUEST['uname'];
+      $firstname = $_REQUEST['fname'];
+      $lastname = $_REQUEST['lname'];
+      $name = $firstname." ".$lastname;
+      $cid = $_REQUEST['cid'];
+      $mob = $_REQUEST['mob'];
+      $dob = $_REQUEST['dob'];
+      $mail = $_REQUEST['mail'];
+      $password = $_REQUEST['psw'];
+      $username_l = $_REQUEST['uname_l'];
+      $password_l = $_REQUEST['psw_l'];
+      $check = !empty($username_l);
+      $PerID = 0;
+      if($check){
+        // Login Form
+        $_SESSION['username'] = $username_l;
+        $sql = "select Password, Username, PerID from User where Username = '" . $username_l . "' and Password = '" . $password_l . "';";
+        $result = $conn->query($sql);
+        if($result->num_rows == 0){
+          echo "Invalid Username and Password!";
+        }
+        else{
+          while($row = $result->fetch_assoc()) {
+            $PerID = $row["PerID"];
+            if ($PerID == 777){
+              // ADMIN
+              header("Location: ./query_admin.php");
+            }
+            elseif ($PerID == 100){
+              // Police
+              header("Location: ./query_police.php");
+            }
+            else{
+              // Normal User
+              header("Location: ./query.php");
+            }
           }
         }
       }
-    }
-    else {
-      // Sign-Up Form
-      $sql = "insert into User (UserID, Name, Mobile, Email, ) values (".$username");";
-      // Check if the result returned is valid or not!
-      if (!mysqli_query($con,$sql)){
-        echo("Error description: " . mysqli_error($con));
+      else{
+        // Sign-Up Form
+        echo $cid;
+        echo $username;
+        echo $password;
+        echo $mail;
+        echo $PerID;
+        echo "<br/>";
+        $sql = "insert into User (AadhaarID, Username, Password, Email, PerID) values (" . $cid . ",'" . $username . "','" . $password . "','" . $mail . "'," . $PerID . ");";
+        echo $sql . "<br/>";
+        if (!mysqli_query($conn,$sql)){
+          echo("Error description: " . mysqli_error($conn));
+        }
       }
     }
-  }
-?>
+  ?>
+
 </body>
 </html>
